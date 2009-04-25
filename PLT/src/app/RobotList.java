6fd6cb21944ca62fb470c04f90eed991a180255d
msukan <msukan@753861ec-fab1-11dd-984d-ab748d24aa7e>
2009-04-25 05:01:23 +0000

    RobotList using generics

--- PLT/src/app/RobotList.java
+++ PLT/src/app/RobotList.java
@@ -2,6 +2,44 @@ package app;
 
 import java.util.*;
 
-public class RobotList <E> extends LinkedList<E> {
+public class RobotList<E> extends LinkedList<E> {
+
+   Class<E> clazz;
+
+   public RobotList(Class<E> cls) {
+      super();
+      clazz = cls;
+   }
+
+   public E get(int index) {
+
+      E item = null;
+      int indexZeroBased = index - 1;
+
+      try {
+         
+         item = super.get(indexZeroBased);
+
+      } catch (IndexOutOfBoundsException e) {
+         
+         try {
+
+            for (int i = this.size(); i <= indexZeroBased; i++) {
+               item = clazz.newInstance();
+               this.add(item);
+            }
+
+         } catch (InstantiationException e2) {
+            e2.printStackTrace();
+         } catch (IllegalAccessException e2) {
+            e2.printStackTrace();
+         }
+
+      } catch (Exception other) {
+         other.printStackTrace();
+      }
+
+      return item;
+   }
 
 }
