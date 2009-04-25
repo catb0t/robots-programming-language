63ce52adb27bc03fcccc923be0d0af8377c23867
soonhac.hong <soonhac.hong@753861ec-fab1-11dd-984d-ab748d24aa7e>
2009-04-25 01:03:47 +0000

    Add test case for grammar

---
+++ PLT/SampleCode/Operators.java
@@ -0,0 +1,69 @@
+import app.*;
+import java.util.*;
+
+public class RobotCompiled extends Robot {
+
+
+	public void think() {
+
+		Float a = 100f;
+		Float b = 50f;
+		Float c = Func.add(a, b);
+		Float d = a - b;
+		Float e = Func.times(a, b);
+		Float f = a / b;
+		
+		if (a<b) {
+		}
+
+		
+		if (a<=b) {
+		}
+
+		
+		if (a>b) {
+		}
+
+		
+		if (a>=b) {
+		}
+
+		
+		if (a==b) {
+		}
+
+		
+		if (a!=b) {
+		}
+
+		
+		if (a<b) {
+		}
+
+		
+		if (a<=b) {
+		}
+
+		
+		if (a>b) {
+		}
+
+		
+		if (a>=b) {
+		}
+
+		
+		if (a==b) {
+		}
+
+		
+		if (a!=b) {
+		}
+
+		Boolean x = true;
+		Boolean y = false;
+
+	}
+
+
+}
