857e81a81267c67d345f554c6d1ac3374e6d4297
msukan <msukan@753861ec-fab1-11dd-984d-ab748d24aa7e>
2009-04-25 05:25:25 +0000

    a couple of more tests

--- PLT/src/app/AppTests.java
+++ PLT/src/app/AppTests.java
@@ -20,4 +20,38 @@ public class AppTests {
       assertTrue(ll.get(5).x == 20f);
 
    }
+
+   @Test
+   public void test_location_equality() {
+      
+      System.out.println("Testing Location.equals()...");
+      
+      Location l1 = new Location();
+      Location l2 = new Location(0f, 0f);
+
+      assertTrue(l1.equals(l2));
+
+      l2.x = 1f;
+
+      assertTrue(!l1.equals(l2));
+
+   }
+
+   @Test
+   public void test_rollover() {
+      
+      System.out.println("Testing Func.rollover()...");
+      
+      float f1;
+      
+      f1 = Func.rollover(1f, 0f); assertTrue(f1 == 1f);
+      f1 = Func.rollover(0f, 4f); assertTrue(f1 == 0f);
+      f1 = Func.rollover(1f, 4f); assertTrue(f1 == 1f);
+      f1 = Func.rollover(2f, 4f); assertTrue(f1 == 2f);
+      f1 = Func.rollover(3f, 4f); assertTrue(f1 == 3f);
+      f1 = Func.rollover(4f, 4f); assertTrue(f1 == 0f);
+      f1 = Func.rollover(5f, 4f); assertTrue(f1 == 1f);
+
+   }
+
 }
