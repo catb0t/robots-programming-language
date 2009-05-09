1b739b0b77ffbe725166af3dcaeac07b11f41b22
soonhac.hong <soonhac.hong@753861ec-fab1-11dd-984d-ab748d24aa7e>
2009-05-09 22:21:35 +0000

    Update test case corresponding Percentage from Percent

--- trunk/PLT/src/SampleCode/Identifiers.java
+++ trunk/PLT/src/SampleCode/Identifiers.java
@@ -4,14 +4,14 @@ import java.util.*;
 public class RobotCompiled extends Robot {
 
 Float wholecount = new Float(0f);
-Percent Maximum_energy_level = new Float(100f);
+Percentage Maximum_energy_level = new Float(100f);
 Boolean isEnemy = false;
 
 	public void think() {
 
 		Float count = new Float(0f);
 		Float count1 = new Float(10.5f);
-		Percent energy_level = new Float(90f);
+		Percentage energy_level = new Float(90f);
 		isEnemy = true;
 		Location my_loc = this.getLocation();
 		Resource closest_resource = new Resource();
